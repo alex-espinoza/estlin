8609c0d257e61d65fcf22b377b866fdfa5851c2a
Alex Espinoza <aespinoza@aspria.net>
2013-06-07 18:33:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estlin::Application.config.secret_token = '02fae8642d145371422a3370c0c957ba268e6cb815b0d5c5413380cb852215420d42b22348ebaccd1e5f4a641941a91a6289b0f5b1fc1251f4bce241ebc4457c'
