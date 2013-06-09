dc24c4f7122274ad834461ac0a0c8223ee36cbf9
Alex Espinoza <aespinoza@aspria.net>
2013-06-09 16:08:01 -0400

    Reading, tweeting and deleting from file tested and working - code and tests refactored.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,55 +5,35 @@ class Tweet < ActiveRecord::Base
   attr_accessible :text
   validates_uniqueness_of :text
 
-  def get_tweet_from_file
-  	filepath = "#{Rails.root}/app/models/test.txt"
-  	status = ""
-		File.open(filepath, "r").each_line do |line|
- 			status = line
+  attr_accessor :filepath, :tweet
+
+  def get_status_from_file
+  	@filepath = "#{Rails.root}/app/models/test.txt"
+  	@tweet = ""
+		File.open(@filepath, "r") do |line|
+ 			@tweet = line.readline.chomp
  		end
- 		return status
+ 		return @tweet
   end
 
-  def get_specific_tweet_from_file(line_num)
-  	filepath = "#{Rails.root}/app/models/test2.txt"
-  	status = ""
-  	counter = 0
-  	File.foreach(filepath) do |line|
-  		counter += 1
-  		if counter == line_num
-  			status = line.chomp
-  			break
-  		end
-  	end
-  	if status != "" then return status else return "Error: That line doesn't exist." end
-  end
-
-  def get_tweet_and_delete_from_file(line_num)
+  def delete_status_and_update_file(status)
   	temp_file = Tempfile.new("update")
-  	filepath = "#{Rails.root}/app/models/test2.txt"
-  	status = ""
-  	counter = 0
-  	File.foreach(filepath) do |line|
-  		counter += 1
-  		if counter == line_num
-  			status = line.chomp
-  			break
-  		end
-  	end
-  	File.foreach(filepath) do |line|
+  	File.foreach(@filepath) do |line|
   		temp_file << line unless line.chomp == status
   	end
   	temp_file.close
-  	FileUtils.mv(temp_file.path, filepath)
-  	if status != "" then return status else return "Error: That line doesn't exist." end
+  	FileUtils.mv(temp_file.path, @filepath)
+  	temp_file.unlink
   end
 
-  def read_text_file
-  	filepath = "#{Rails.root}/app/models/test2.txt"
-  	File.open(filepath, "r") do |f|
-  		while line = f.gets
-  			puts line
-  		end
+  def post_status
+  	if @tweet != "" && @tweet != nil
+  		Twitter.update(@tweet)
+  		Tweet.create(:text => @tweet)
+  		@tweet = ""
+  	else
+  		puts "ERROR: Status has not been updated due to tweet not being a string."
   	end
   end
+
 end
--- spec/features/estlin_spec.rb
+++ spec/features/estlin_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
  describe 'Estlin' do
+
  	it 'displays the last status that was tweeted.' do
- 		t = Tweet.new
-		tweet = t.get_tweet_from_file
-		t.text = tweet
-		t.save
+ 		s = Tweet.new
+ 		tweet = s.get_status_from_file
+ 		s.delete_status_and_update_file(tweet)
+		s.post_status
  		visit "/"
- 		expect(page).to have_content("The third tweet.")
+ 		expect(page).to have_content("tweet")
+ 		expect(Tweet.last.text).to eq("The first tweet.")
  	end
 
  end
