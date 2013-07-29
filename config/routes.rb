2d0ed3a83d53ee22696fde0bd9a33cd724016f7d
Alex Espinoza <aespinoza@aspria.net>
2013-07-29 15:05:44 -0400

    Tweet scheduling form created, tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Estlin::Application.routes.draw do
   resources :tweets
 
   root :to => "tweets#index"
-  match "/auth/twitter/callback", to: "sessions#create", as: "signin"
+  match "/auth/twitter", as: "signin"
+  match "/auth/twitter/callback", to: "sessions#create"
   match "/signout", to: "sessions#destroy", as: "signout"
   match "/auth/failure", to: "sessions#failed", as: "failed_authentication"
 end
--- spec/features/tweet_spec.rb
+++ spec/features/tweet_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe 'Tweet' do
 	let!(:twitter_user) { FactoryGirl.create(:twitter_user) }
 
-	it 'input text box should only be visible when logged in.' do
+	it 'input form should only be visible when logged in.' do
 		visit root_path
 		mock_omniauth_sign_in
 		click_link "Sign in with Twitter"
 		expect(page).to have_button("Schedule Tweet")
 	end
 
-	it 'input text box should not be visible if not logged in.' do
+	it 'input form should not be visible if not logged in.' do
 		visit root_path
 		expect(page).to_not have_button("Schedule Tweet")
 	end
