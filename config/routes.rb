7cf9dc4ee630ba0b89fbb778e9035ae5d2450241
Alex Espinoza <aespinoza@aspria.net>
2013-07-28 20:04:16 -0400

    Root changed to tweets#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Estlin::Application.routes.draw do
   resources :users
   resources :tweets
 
-  root :to => "users#index"
+  root :to => "tweets#index"
   match "/auth/twitter/callback", to: "sessions#create", as: "signin"
   match "/signout", to: "sessions#destroy", as: "signout"
   match "/auth/failure", to: "sessions#failed", as: "failed_authentication"
