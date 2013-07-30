97fbbd8076e43eba01fc0227323f4c2029ec53bc
Alex Espinoza <aespinoza@aspria.net>
2013-07-30 19:46:07 -0400

    User model updated for tweeting

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,25 @@ class User < ActiveRecord::Base
 
 	validates_presence_of :uid
   validates_presence_of :nickname
+  validates_presence_of :oauth_token
+  validates_presence_of :oauth_secret
 
   def self.from_omniauth(auth)
-  	User.where(auth.slice("uid")).first || create_from_omniauth(auth)
+  	user = User.where(auth.slice("uid")).first || create_from_omniauth(auth)
+    if user.oauth_token != auth["credentials"]["token"] || user.oauth_token != auth["credentials"]["secret"]
+      user.oauth_token = auth["credentials"]["token"]
+      user.oauth_secret = auth["credentials"]["secret"]
+      user.save!
+    end
+    user
   end
 
   def self.create_from_omniauth(auth)
   	create! do |user|
   		user.uid = auth["uid"]
   		user.nickname = auth["info"]["nickname"]
+      user.oauth_token = auth["credentials"]["token"]
+      user.oauth_secret = auth["credentials"]["secret"]
   	end
   end
 end
