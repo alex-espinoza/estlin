21308e57a17c9991bdedccfe2b344503b4e28ed7
Alex Espinoza <aespinoza@aspria.net>
2013-07-30 18:53:55 -0400

    Actual tweeting of scheduled tweets feature tests created

--- spec/features/tweet_spec.rb
+++ spec/features/tweet_spec.rb
@@ -3,31 +3,13 @@ require 'spec_helper'
 describe 'Tweet' do
 	let!(:twitter_user) { FactoryGirl.create(:twitter_user) }
 
-	it 'input form should only be visible when logged in.' do
+	def valid_sign_in_via_twitter
 		visit root_path
 		mock_omniauth_sign_in
 		click_link "Sign in with Twitter"
-		expect(page).to have_button("Schedule Tweet")
 	end
 
-	it 'input form should not be visible if not logged in.' do
-		visit root_path
-		expect(page).to_not have_button("Schedule Tweet")
-	end
-
-	it 'input form is invalid if any fields are not filled in.' do
-		visit root_path
-		mock_omniauth_sign_in
-		click_link "Sign in with Twitter"
-		click_button "Schedule Tweet"
-		expect(page).to have_content("can't be blank")
-		expect(Tweet.all.count).to be(0)
-	end
-
-	it 'input form is valid if all fields are filled in.' do
-		visit root_path
-		mock_omniauth_sign_in
-		click_link "Sign in with Twitter"
+	def valid_tweet_form_input
 		fill_in "Text", :with => "This is a test scheduled tweet."
 		select "2013", :from => "tweet_scheduled_time_1i"
 		select "August", :from => "tweet_scheduled_time_2i"
@@ -35,7 +17,60 @@ describe 'Tweet' do
 		select "18", :from => "tweet_scheduled_time_4i"
 		select "00", :from => "tweet_scheduled_time_5i"
 		click_button "Schedule Tweet"
+	end
+
+	describe '- input form -' do
+		it 'input form should only be visible when logged in.' do
+			valid_sign_in_via_twitter
+			expect(page).to have_button("Schedule Tweet")
+		end
+
+		it 'input form should not be visible if not logged in.' do
+			visit root_path
+			expect(page).to_not have_button("Schedule Tweet")
+		end
+
+		it 'input form is invalid if any fields are not filled in.' do
+			valid_sign_in_via_twitter
+			click_button "Schedule Tweet"
+			expect(page).to have_content("can't be blank")
+			expect(Tweet.all.count).to be(0)
+		end
+
+		it 'input form is valid if all fields are filled in.' do
+			valid_sign_in_via_twitter
+			valid_tweet_form_input
+			expect(page).to have_content("Tweet has been scheduled")
+			expect(Tweet.all.count).to be(1)
+		end
+	end
+
+	it 'will be added to delayed_job process queue when scheduled.' do
+		valid_sign_in_via_twitter
+		valid_tweet_form_input
 		expect(page).to have_content("Tweet has been scheduled")
-		expect(Tweet.all.count).to be(1)
+		expect(Delayed::Job.count).to eq(1)
+		expect(Delayed::Job.last.handler).to have_content("This is a test scheduled tweet.")
+	end
+
+	describe '- will be tweeted -' do
+		before(:each) do
+			test_time = Time.utc(2013, 8, 1, 18, 0, 0)
+			Timecop.freeze(test_time)
+		end
+
+		after(:each) { Timecop.return }
+
+		it 'only at the time specified by its scheduled_time column.' do
+			valid_sign_in_via_twitter
+			valid_tweet_form_input
+			expect(Delayed::Job.count).to eq(1)
+			scheduled_time_of_test_tweet = Time.utc(2013, 8, 4, 18, 0, 0)
+			Timecop.freeze(Time.now + 3.days)
+			expect(Tweet.first.scheduled_time).to eq(scheduled_time_of_test_tweet)
+			expect(Tweet.first.scheduled_time).to eq(Time.now)
+			expect(Delayed::Job.count).to eq(0)
+			expect(Tweet.first.was_tweeted).to eq(true)
+		end
 	end
 end
