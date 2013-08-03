a9e4160cf2ac81e4a0c1f877fe1eaa4518bc12e2
Alex Espinoza <aespinoza@aspria.net>
2013-08-03 14:33:23 -0400

    ENV variables changed to easily work between test/development and production environments

--- config/initializers/omniauth_twitter.rb
+++ config/initializers/omniauth_twitter.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+	if Rails.env.production?
+	  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+	else
+		provider :twitter, ENV['TEST_TWITTER_CONSUMER_KEY'], ENV['TEST_TWITTER_CONSUMER_SECRET']
+	end
 end
 
 OmniAuth.config.logger = Rails.logger
--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,9 @@
 Twitter.configure do |config|
-  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+	if Rails.env.production?
+	  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+	  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+	else
+		config.consumer_key = ENV['TEST_TWITTER_CONSUMER_KEY']
+	  config.consumer_secret = ENV['TEST_TWITTER_CONSUMER_SECRET']
+	end
 end
